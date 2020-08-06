9a199953f44dc09cb8d405b6f6a7ba7293af0595
albertcoder <eralbert9191@gmail.com>
2020-08-06 01:15:31 -0700

    added another solution

--- topKelements.java
+++ topKelements.java
@@ -1,6 +1,6 @@
-//time complexity O(n)
+//time complexity O(nlogk)
 //space complexity O(n) size of hashmap. priority queue and result array can be neglected because k is constant and negligible
-
+//APPROACH 1: using priority queue, hashmap and result array
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -19,6 +19,36 @@ class Solution {
         }
         for(int i = 0; i < k; i++){
             result[result.length -1 - i] = pq.poll();
+        }
+        return result;
+    }
+}
+
+//time complexity O(n)
+//space complexity O(2n)
+//APPROACH 2: without using priority queue
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[k];
+        List<Integer> [] buckets = new List[nums.length + 1];//because in edge case of only one element freq will be 1 i.e. the size atleast need to be 1
+        for(int num: nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for(int key : map.keySet()){
+            int f = map.get(key);
+            if(buckets[f] == null){
+                buckets[f] = new ArrayList<>();
+            }
+            buckets[f].add(key);
+        }
+        for(int i = buckets.length - 1; i >= 0 && k > 0; i--){
+            if(buckets[i] != null){
+                for(int j = 0; j < buckets[i].size() && k > 0; j++){
+                    result[result.length - k] = buckets[i].get(j);
+                    k--;
+                }
+            }
         }
         return result;
     }

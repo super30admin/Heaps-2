da04d5547d1abbf985143540cab08971d83020b8
Sai Vinherkar <sai.vinherkar@angi.com>
2023-11-07 22:42:54 -0600

    Complete-Heaps-2

---
+++ TopKFrequent.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map  = new HashMap<>();
+        for(int num: nums){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> map.get(a)-map.get(b));
+        for(Integer key : map.keySet()){
+            pq.add(key);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+
+        int res[] = new int[k];
+        int i = 0;
+        while(!pq.isEmpty()){
+            res[i] = pq.poll();
+            i++;
+        }
+        return res;
+    }
+}
